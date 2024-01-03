30efd3202ba46f42e74a9ddcf0e5c58d514a4d77
do.sg <do.seongyeong1@gmail.com>
2024-01-03 17:31:49 +0900

    docs : 최초 2차 협업 프로젝트 환경구성

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Wrap } from "./styles/basic";
+
+const App = () => {
+  return (
+    <Wrap maxw={960}>
+      <Routes>
+        {/* <Route path="/intro" element={<Intro />}></Route> */}
+        <Route path="*" element={<h1>파일이없네요.</h1>}></Route>
+      </Routes>
+    </Wrap>
+  );
+};
+
+export default App;
