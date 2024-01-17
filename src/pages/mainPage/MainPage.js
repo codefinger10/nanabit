29c8b68abf94680e2d6f9fd71359739880e0770d
zuicy99 <k.zuicy@gmail.com>
2024-01-17 18:18:56 +0900

    fix : 오류수정

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,12 +1,12 @@
 import React from "react";
-import MainRcSwiper from "../../components/mainpagecomponent/MainRcSwiper";
-import MainSwiper from "../../components/mainpagecomponent/MainSwiper";
+// import MainRcSwiper from "../../components/mainpagecomponent/MainRcSwiper";
+// import MainSwiper from "../../components/mainpagecomponent/MainSwiper";
 
 const MainPage = () => {
   return (
     <div>
-      <MainSwiper />
-      <MainRcSwiper />
+      {/* <MainSwiper /> */}
+      {/* <MainRcSwiper /> */}
     </div>
   );
 };
