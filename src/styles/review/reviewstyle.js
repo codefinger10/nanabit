171ca9dcf6b0323dc6e403e84ae2ba00b2612d14
Phowebing <phowebing@gmail.com>
2024-01-30 18:13:55 +0900

    docs : MainPage 데이터 연동

--- src/styles/review/reviewstyle.js
+++ src/styles/review/reviewstyle.js
@@ -54,11 +54,8 @@ export const ReviewList = styled.div`
     justify-content: space-between;
     text-align: center;
     line-height: 50px;
-    span {
+    p {
       color: #868686;
-    }
-    b {
-      color: #e9b25f;
     }
   }
   .productName {
@@ -75,6 +72,7 @@ export const ReviewList = styled.div`
       text-overflow: ellipsis;
       font-size: 20px;
       color: #868686;
+      text-align: right;
     }
     i {
       font-size: 30px;
@@ -118,4 +116,90 @@ export const ReviewFooter = styled.div`
   margin-bottom: 40px;
   width: 1150px;
   height: 80px;
+`;
+
+// ReviewAddCom.js
+
+export const ReviewAddWrap = styled.div`
+  width: 1440px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  i {
+    font-size: 12px;
+    font-style: normal;
+    color: #42b0ff;
+  }
+  .productInfo {
+    display: flex;
+    justify-content: space-between;
+    gap: 40px;
+    img {
+      width: 170px;
+      height: 170px;
+    }
+  }
+  .productInfoText {
+    width: 930px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    p {
+      width: 930px;
+      font-size: 20px;
+      line-height: 30px;
+    }
+  }
+  .ant-input-data-count {
+    bottom: -40px;
+  }
+  .antdUploadDiv {
+    margin-top: 60px;
+    display: "flex";
+  }
+  .buttonDiv {
+    width: 140px;
+    height: 140px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+`;
+
+export const ReviewAddTitle = styled.div`
+  width: 1150px;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  span {
+    color: #e9b25f;
+    font-size: 70px;
+    font-weight: 500;
+    margin-bottom: 10px;
+  }
+  p {
+    color: #c5c5c5;
+    font-size: 30px;
+  }
+`;
+
+export const RateAddBox = styled.div`
+  margin-top: 30px;
+  margin-bottom: 30px;
+  width: 1150px;
+  height: 100px;
+  border: 1px solid #e9b25f;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 35px 40px;
+  p {
+    font-size: 30px;
+    font-weight: 600;
+    color: #e9b25f;
+  }
+  // Rate .. 색이 바뀐다
+  /* span {
+    color: #E9B25F;
+  } */
 `;
